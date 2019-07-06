8bb561ec845fd8fba27abb294e69ab1381937939
Arun Shinde <arunshinde@hotmail.com>
2019-07-06 19:28:38 -0400

    Finished Chapter 2 for stocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StocksService } from './services/stocks.service';
+import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/stocks.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR', 'PRU'];
+const service = 'https://angular2-in-action-api.herokuapp.com';
+
+export interface StockInterface {
+  symbol: string;
+  lastTradePriceOnly: number;
+  change: number;
+  changeInPercent: number;
+}
+
+@Injectable()
+export class StocksService {
+  constructor(private http: HttpClient) { }
+  get() {
+    return stocks.slice();
+  }
+
+  add(stock) {
+    stocks.push(stock);
+    return this.get();
+  }
+
+  remove(stock) {
+    stocks.splice(stocks.indexOf(stock), 1);
+    return this.get();
+  }
+
+  load(symbols) {
+    if (symbols) {
+      return this.http.get<Array<StockInterface>>(service + '/stocks/snapshot?symbols=' + symbols.join());
+    }
+  }
+}
